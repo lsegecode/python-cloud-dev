5ba4d16715248bc977b2730129b39276ed5e939f
Lucas Sanabria <sanabrialucas97@gmail.com>
2025-05-14 20:56:55 -0300

    added user

--- fastapi-project/app/main.py
+++ fastapi-project/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from app.routers import hello, user
+from app.models.user import Base
+from app.database import engine
 
 app = FastAPI()
 
-app.include_router(hello.router)
-app.include_router(user.router)
+@app.on_event("startup")
+def startup_event():
+    Base.metadata.create_all(bind=engine)
+
+app.include_router(user.router, prefix="/users", tags=["Users"])
+app.include_router(hello.router, tags=["Hello"])
+
+
--- fastapi-project/app/routers/user.py
+++ fastapi-project/app/routers/user.py
@@ -1,11 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.schemas.user import UserCreate
+from app.models.user import User
+from app.dependencies import get_db
+
 
 router = APIRouter()
 
-@router.get("/user")
-def read_root():
-    return {"message": "¡Hola desde FastAPI, Lucas!"}
+@router.get("/{user_id}")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        return user
+    return {"error": "User not found"}
 
-@router.get("/ping2")
-def ping():
-    return {"a": "pong"}
+
+
+@router.post("/")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
